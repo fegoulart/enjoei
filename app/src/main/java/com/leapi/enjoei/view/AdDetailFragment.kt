899581ac2e09989f2dfc3702548d18b7704d5ad0
Fernando Luiz Goulart <fernando@leapi.com>
2021-03-01 01:47:44 -0300

    detail ok

--- app/src/main/java/com/leapi/enjoei/view/AdDetailFragment.kt
+++ app/src/main/java/com/leapi/enjoei/view/AdDetailFragment.kt
@@ -1,5 +1,6 @@
 package com.leapi.enjoei.view
 
+import android.content.res.Resources
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,8 +8,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.navigation.fragment.NavHostFragment
+import com.bumptech.glide.Glide
 import com.leapi.enjoei.R
 import com.leapi.enjoei.databinding.AdDetailFragmentBinding
 import com.leapi.enjoei.di.util.fromHtml
@@ -35,6 +38,8 @@ class AdDetailFragment : Fragment() {
     lateinit var priceConditionsTextView: TextView
     lateinit var availableItemsTextView: TextView
     lateinit var soldItemsTextView: TextView
+    lateinit var storeAvatarImageView: ImageView
+    lateinit var adPhoto: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -72,6 +77,8 @@ class AdDetailFragment : Fragment() {
         priceConditionsTextView = binding.priceCondition
         soldItemsTextView = binding.soldCount
         availableItemsTextView = binding.availableCount
+        storeAvatarImageView = binding.storeLogo
+        adPhoto = binding.adImage
     }
 
     private fun setupBackButton() {
@@ -130,6 +137,13 @@ class AdDetailFragment : Fragment() {
             brandTextView.text = "-"
         }
 
+        val avatarUrl = "https://photos.enjoei.com.br/public/75x75/${basicAdData.storeAvatar}.jpg"
+
+        Glide.with(this)
+            .load(avatarUrl)
+            .circleCrop()
+            .into(storeAvatarImageView)
+
     }
 
     private fun updateAdditionalDataUI(additionalAdData: AdditionalAdData) {
@@ -138,6 +152,30 @@ class AdDetailFragment : Fragment() {
             true -> productConditionTextView.text = getString(R.string.usedProduct)
             false -> productConditionTextView.text = getString(R.string.newProduct)
         }
+
+        val screenWidth = Resources.getSystem().displayMetrics.widthPixels
+
+        val imageSize: String = if (screenWidth <= 240) {
+            "240x240"
+        } else {
+            if (screenWidth <= 460) {
+                "460x460"
+            } else {
+                if (screenWidth <= 500) {
+                    "500x500"
+                } else {
+                    "1200x1200"
+                }
+            }
+        }
+
+        val imageUrl =
+            "https://photos.enjoei.com.br/public/${imageSize}/${additionalAdData.photos[0]}.jpg"
+
+        Glide.with(this)
+            .load(imageUrl)
+            .centerCrop()
+            .into(adPhoto)
     }
 
     private fun updatePricingDataUI(pricingData: AdPricingData) {
